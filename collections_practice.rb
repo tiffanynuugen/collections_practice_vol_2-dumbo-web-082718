5a24136e2c1eb04a63d9cb608a664d3dd702cb3e
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-17 19:57:47 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,33 @@
-# your code goes here
+array = ["ruby", "rspec", "rails"]
+array2 = ["ruby", "rspec", "sails"]
+
+ def begins_with_r(array)
+   i = 0
+   while i < array.length
+   all = array.all?
+   if array[i].start_with?('r')
+     puts true
+     i += 1
+   else
+     puts false
+     i += 1
+   end
+ end
+
+def begins_with_r(element)
+  if element.start_with?("r")
+     true
+  else
+     false
+  end
+end
+
+def begins_with_r(array2)
+ array2.all? {|word| word.start_with?("r")}
+ array2.each do |name|
+   if name.all? { |name| name.start_with?("r") }
+     true
+   else
+   end
+   begins_with_r(array2)
+ end
